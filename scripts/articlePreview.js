df12f059de8a727e06bd5288313d6f770069b642
Sabrina Tee <sabrinatee@me.com>
2015-12-04 15:28:03 -0800

    Added highlight.js and marked libraries

--- scripts/articlePreview.js
+++ scripts/articlePreview.js
@@ -1,6 +1,13 @@
 // $('#articles').html(marked('I am using __markdown__.'));
-
 $('#article-body').keypress(function(){
   var $articlePreview = $('#article-body').val();
   $('#articles').html(marked($articlePreview));
+});
+
+
+$(document).ready(function() {
+  hljs.configure({useBR: true});
+  $('#articles').each(function(i, block) {
+    hljs.highlightBlock(block);
+  });
 });
